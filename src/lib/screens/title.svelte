64c579cb0f9d9b5238dfa40927617143f6465417
macoshita <macoshita@users.noreply.github.com>
2022-01-22 16:47:21 +0900

    Add GitHub link

--- src/lib/screens/title.svelte
+++ src/lib/screens/title.svelte
@@ -2,6 +2,7 @@
   import Paper from '$lib/components/paper.svelte';
   import Button from '$lib/components/button.svelte';
   import { scene } from '$lib/stores';
+  import GithubLogo from '$lib/components/github-logo.svelte';
 
   function start() {
     scene.set('play');
@@ -13,4 +14,9 @@
     Rebuild.fm<br />タイトルクイズ
   </h1>
   <Button on:click={start}>開始</Button>
+  <div class="block mt-16 text-center">
+    <a href="https://github.com/macoshita/rebuildfm-title-quiz">
+      <GithubLogo class="w-8 inline" />
+    </a>
+  </div>
 </Paper>
